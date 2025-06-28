6c599f8ea904783011c20e68bb022d70b83332d8
Iqbal Mahdi <mahdiiqbal37@gmail.com>
2025-06-28 20:52:56 +0600

    update: caching mechanism initiated and extension zip file added

--- api.ts
+++ api.ts
@@ -1,6 +1,10 @@
 import express from "express"
-import Browsers from "./browsers/browsers.ts"
 import fs from "fs"
+import Keyv from "keyv"
+import { KeyvFile } from "keyv-file"
+
+import Browsers from "./browsers/browsers.ts"
+import { getFilename, getFilepath, runCleanup } from "./utils.ts"
 
 const app = express()
 
@@ -8,6 +12,16 @@ const port = process.env.PORT || 3000
 
 const extensionPath = await fs.promises.realpath("./extensions/ad-block-plus")
 const browsers = new Browsers(extensionPath)
+
+if ( !fs.existsSync("./cache") ) {
+    fs.mkdirSync("./cache")
+    fs.mkdirSync("./cache/html")
+    fs.mkdirSync("./cache/screenshot")
+}
+
+const keyv = new Keyv( {
+    store: new KeyvFile( { filename: "./cache/keyv-file.json" } )
+} )
 
 app.get("/", async (req, res) => {
     res.send("Hello!")
@@ -18,15 +32,30 @@ app.get("/html", async (req, res) => {
     const blockAds = req.query.block_ads
     const useProxy = req.query.use_proxy
 
+    const filename = getFilename(url)
+    const cacheKey = `html_${filename}`
+    let filepath = await keyv.get(cacheKey)
+
+    if ( filepath ) {
+        const HTMLContent = await fs.promises.readFile( filepath, "utf-8" )
+        res.send(HTMLContent)
+        return
+    }
+
     const browser = await browsers.acquire(useProxy, blockAds)
 
     const page = await browser.newPage()
     await page.bringToFront()
-    await page.goto(url, { waitUntil: "networkidle2" })
+    await page.goto(url, { waitUntil: "networkidle2", timeout: 60_000 })
     const HTMLContent = await page.content()
     await page.close()
 
     res.send(HTMLContent)
+
+    filepath = getFilepath(filename, "html")
+    await fs.promises.writeFile(filepath, HTMLContent)
+
+    await keyv.set(cacheKey, filepath, 1000 * 60 * 60 * 4)
 
     await browsers.release(useProxy, blockAds, browser)
 })
@@ -38,6 +67,16 @@ app.get("/screenshot", async (req, res) => {
     const width = Number( req.query.width ) || 1920
     const height = Number( req.query.height ) || 1080
 
+    const filename = getFilename(url)
+    const cacheKey = `screenshot_${filename}`
+    let filepath = await keyv.get(cacheKey)
+
+    if ( filepath ) {
+        const fullpath = await fs.promises.realpath(filepath)
+        res.setHeader("Content-Type", "image/png").sendFile(fullpath)
+        return
+    }
+
     const browser = await browsers.acquire(useProxy, blockAds)
 
     const page = await browser.newPage()
@@ -47,7 +86,12 @@ app.get("/screenshot", async (req, res) => {
     const ss = await page.screenshot({ fullPage: true })
     await page.close()
 
-    res.set("Content-Type", "image/png").send(ss)
+    res.setHeader("Content-Type", "image/png").send(ss)
+
+    filepath = getFilepath(filename, "screenshot")
+    await fs.promises.writeFile(filepath, ss, "binary")
+
+    await keyv.set(cacheKey, filepath, 1000 * 60 * 60 * 4)
 
     await browsers.release(useProxy, blockAds, browser)
 })
@@ -55,3 +99,5 @@ app.get("/screenshot", async (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+await runCleanup(keyv)
---
+++ utils.ts
@@ -0,0 +1,42 @@
+import fs from "fs"
+import Keyv from "keyv"
+
+function getFilename(url: string) {
+    return url.replace(/^https?:\/\//, "").replace(/^www./, "").replace(/\/$/, "").replace(/[^a-zA-Z0-9]/g, "_")
+}
+
+function getFilepath( filename: string, filetype: string ) {
+    let fileExt = "png"
+
+    if ( filetype === "html" ) fileExt = "html"
+
+    return `./cache/${filetype}/${filename}.${fileExt}`
+}
+
+async function runCleanup(keyv: Keyv) {
+    while ( true ) {
+        let i = 0
+        console.log("Clearing cache...")
+
+        for ( let filetype of [ "html", "screenshot" ] ) {
+            const files = await fs.promises.readdir(`./cache/${filetype}`)
+
+            await Promise.all(
+                files.map(async filename => {
+                    const cacheKey = `${filetype}_${filename}`.replace(`.${filetype === "html" ? "html" : "png"}`, "")
+                    const exists = await keyv.has(cacheKey)
+
+                    if ( !exists )
+                        await fs.promises.unlink(`./cache/${filetype}/${filename}`).then(() => i++)
+
+                })
+            )
+        }
+
+        console.log(`Cleared cache: ${i} files removed.`)
+
+        await new Promise(resolve => setTimeout(resolve, 10_000))
+    }
+}
+
+export { getFilename, getFilepath, runCleanup }
